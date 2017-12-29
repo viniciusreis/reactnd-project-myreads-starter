93c996b023af7e330b75b5e6b1fec8709fd294ec
Vinícius da Costa Reis <vreis@devranger.io>
2017-12-29 18:49:48 -0200

    Sets sign in button to trigger modal panel

--- src/Components/ListBooks/NavegationBar/SidePanel/index.js
+++ src/Components/ListBooks/NavegationBar/SidePanel/index.js
@@ -7,15 +7,15 @@ class SidePanel extends Component {
             <ul className="sidenav front-element" id="slide-out">
                 <li><div className="user-view">
                         <div className="background">
-                            <img src="http://lorempixel.com/300/300/abstract/" />
+                            <img src="http://lorempixel.com/300/300/abstract/" alt=""/>
                         </div>
-                        <a href="#!user"><img className="circle" src="http://lorempixel.com/300/300/people/" /></a>
+                        <a href="#!user"><img className="circle" src="http://lorempixel.com/300/300/people/" alt="" /></a>
                         <a href="#!name"><span className="white-text name">Jurema Jacutinga</span></a>
                         <a href="#!email"><span className="white-text email">jjacutinga@gmail.com</span></a>
                 </div></li>
                 <li><a href="#filter" className="waves-effect"><i className="fa fa-filter icon-menu"/>Filter</a></li>
                 <li><a href="#import" className="waves-effect"><i className="fa fa-cloud-download icon-menu"/>Import</a></li>
-                <li><a href="#signin" className="waves-effect"><i className="fa fa-sign-in icon-menu icon-menu"/>Sign In</a></li>
+                <li><a href="#signin" className="waves-effect modal-trigger" data-target="modal1"><i className="fa fa-sign-in icon-menu icon-menu"/>Sign In</a></li>
             </ul>
         )
     }
--- src/Components/ListBooks/NavegationBar/index.js
+++ src/Components/ListBooks/NavegationBar/index.js
@@ -7,21 +7,21 @@ class Navegation extends Component {
     render(){
         return(
             <div className="navbar-fixed">
-            <nav className="default-primary-color">
-                <div className="nav-wrapper">
-                    <a href="#!" className="brand-logo"><i className="fa fa-book icon-menu hide-on-small-only"/>MyReads</a>
-                    <a href="#" className="sidenav-trigger" data-target="slide-out">
-                        <i className="fa fa-bars icon-menu"></i>
-                    </a>
-                    <ul className="right hide-on-med-and-down">
-                        <li><a href="#filter" className="waves-effect"><i className="waves-effect fa fa-filter icon-menu"/>Filter</a></li>
-                        <li><a href="#import" className="waves-effect"><i className="fa fa-cloud-download icon-menu"/>Import</a></li>
-                        <li><a href="#signin" className="waves-effect"><i className="fa fa-sign-in icon-menu"/>Sign In</a></li>
-                    </ul>
-                    <SidePanel />
-                </div>
-            </nav>
-        </div>
+                <nav className="default-primary-color">
+                    <div className="nav-wrapper">
+                        <a href="#!" className="brand-logo"><i className="fa fa-book icon-menu hide-on-small-only"/>MyReads</a>
+                        <a href="#" className="sidenav-trigger" data-target="slide-out">
+                            <i className="fa fa-bars icon-menu"></i>
+                        </a>
+                        <ul className="right hide-on-med-and-down">
+                            <li><a href="#filter" className="waves-effect"><i className="fa fa-filter icon-menu"/>Filter</a></li>
+                            <li><a href="#import" className="waves-effect"><i className="fa fa-cloud-download icon-menu"/>Import</a></li>
+                            <li><a href="#signin" className="modal-trigger" data-target="modal1"><i className="fa fa-sign-in icon-menu"/>Sign In</a></li>
+                        </ul>
+                        <SidePanel />
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
